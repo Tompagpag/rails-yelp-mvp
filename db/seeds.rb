c34e43c78c40fcf8f707d23ce80f62cbf09105a0
Tom Paganel <paganel.tom@gmail.com>
2021-11-10 15:31:28 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese", phone_number: "+33676452354" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian", phone_number: "+33676098354" }
+lao = { name: "lao", address: "16 rue Custine 75018 Paris", category: "japanese", phone_number: "+33600962354" }
+la_brigade =  { name: "La Brigade", address: "101 rue Oberkampf 75011 Paris", category: "french", phone_number: "+33676090956" }
+frites_alors =  { name: "Frites alors !", address: "12 rue des bouchons 69001 Lyon", category: "belgian", phone_number: "+33776092356" }
+
+[dishoom, pizza_east, lao, la_brigade, frites_alors].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
